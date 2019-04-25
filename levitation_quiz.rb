291dcafd208febd6a1c4cfd938c343f7315e3e36
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-04-25 22:42:29 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -5,8 +5,8 @@ def levitation_quiz
         answer = gets.chomp
         break if answer == "Wingardium Leviosa"
     end
-    puts "Okay, okay, jeez. I'll stop. Sorry."
-end
+    puts "You passed the quiz!"
+
 end
 
 
